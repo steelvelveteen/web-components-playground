cae125b75d13bcb7111490268191e082859f1f9c
steelvelveteen <joeyvico@gmail.com>
2020-11-20 08:09:29 +1030

    Completed a little practical exercise on dom manipulation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import './style.scss';
-
 // To add items to the list we'll first add a submit event
 const form = document.querySelector('#addForm');
-
 // We'll also get the item list and store it
 const itemList = document.querySelector('#items');
+// Filter / search
+const filter = document.querySelector('#filter');
+
 // Form submit event
 form?.addEventListener('submit', (e: Event) => {
+    addItem(e);
+});
+
+const addItem = (e: Event) => {
     e.preventDefault();
     // Get input value
     const newItem = (document.querySelector('#item') as HTMLInputElement).value;
@@ -25,22 +30,38 @@ form?.addEventListener('submit', (e: Event) => {
     // Append the button to the appended li element
     li.appendChild(deleteBtn);
     itemList?.appendChild(li);
-});
-
+};
 // The remove button on each item in the list
 // We'll add a 'click' event to each item in that list
-const itemsList = document.querySelector('#items');
-itemsList?.addEventListener('click', (e: Event) => {
+itemList?.addEventListener('click', (e: Event) => {
     removeItem(e);
 });
 // Remove item function
 const removeItem = (e: Event) => {
-    console.log((e.target as HTMLElement).classList);
+    // console.log((e.target as HTMLElement).classList);
     let el = e.target as HTMLElement;
     if (el.classList.contains('delete')) {
         if (confirm('Are you sure?')) {
             let li = el.parentElement as Node;
-            itemsList?.removeChild(li);
+            itemList?.removeChild(li);
         }
     }
-}
+};
+
+// Working with the filter
+filter?.addEventListener('input', (e: Event) => {
+    let terms = (e.target as HTMLInputElement).value.toLowerCase();
+    // if (terms.length > 2) {
+    // const items = itemList?.querySelectorAll('li');
+    const items = itemList?.getElementsByTagName('li') as ArrayLike<HTMLLIElement>;
+
+    Array.from(items).forEach(item => {
+        let itemName = item.firstChild?.textContent;
+        if (itemName?.toLowerCase().indexOf(terms) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+    // }
+});
