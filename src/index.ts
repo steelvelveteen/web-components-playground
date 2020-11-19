5969e4905b01c48f41063596fea36a1279fba5f2
steelvelveteen <joeyvico@gmail.com>
2020-11-19 17:34:25 +1030

    Dom manipulation exercise first part. Typescript was the challenge here

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
 import './style.scss';
+
+// To add items to the list we'll first add a submit event
+const form = document.querySelector('#addForm');
+
+// We'll also get the item list and store it
+const itemList = document.querySelector('#items');
+// Form submit event
+form?.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    // Get input value
+    const newItem = (document.querySelector('#item') as HTMLInputElement).value;
+
+    // Create new li element
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    // Create the delete button element
+    const deleteBtn = document.createElement('button') as HTMLElement;
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // Append the button to the appended li element
+    li.appendChild(deleteBtn);
+    itemList?.appendChild(li);
+});
+
+// The remove button on each item in the list
+// We'll add a 'click' event to each item in that list
+const itemsList = document.querySelector('#items');
+itemsList?.addEventListener('click', (e: Event) => {
+    removeItem(e);
+});
+// Remove item function
+const removeItem = (e: Event) => {
+    console.log((e.target as HTMLElement).classList);
+    let el = e.target as HTMLElement;
+    if (el.classList.contains('delete')) {
+        if (confirm('Are you sure?')) {
+            let li = el.parentElement as Node;
+            itemsList?.removeChild(li);
+        }
+    }
+}
