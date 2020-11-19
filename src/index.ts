251d7f9638aa3aa435f5bb54b0af1e33e07fb3d3
steelvelveteen <joeyvico@gmail.com>
2020-11-19 10:37:15 +1030

    No big  deal here to remember. Just some examples and a small list of events available

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 import './style.scss';
+
+const button = document.querySelector('#button');
+// console.log(button);
+button?.addEventListener('click', (e: Event) => {
+    // console.log('Button was clicked');
+    const main = document.querySelector('#main') as HTMLElement;
+    main.style.backgroundColor = '#F4F4F4';
+
+    // const main2 = <HTMLElement>document.querySelector('#main');
+    // main2.style.backgroundColor = '#F4F4F4';
+    console.log(typeof e);
+});
